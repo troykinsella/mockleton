8ba7c30b8b45470d78daae58396e63baa574bf24
Troy Kinsella <troy.kinsella@startmail.com>
2018-10-12 11:53:56 -0700

    Make prefix dir.

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ export ASSET=mockleton_$(uname | tr '[:upper:]' '[:lower:]')_amd64
 bash <(wget -q -O - https://gist.githubusercontent.com/troykinsella/b0cf38006d6ff4afb5056129d14f0738/raw/fetch_gh_release.sh)
 
 DEST=$PREFIX/bin/mockleton
+mkdir -p $(dirname $DEST)
 mv $ASSET $DEST
 chmod +x $DEST
 
